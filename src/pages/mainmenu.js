63f26d9f26643b77c675bea6238c613d719c9fdd
Jayden <jduncan9@stu.jsu.edu>
2024-03-04 18:15:12 -0600

    Designed main menu page

--- src/pages/mainmenu.js
+++ src/pages/mainmenu.js
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/fontawesome-free-solid";
 
 function MainMenu(){
     return (
         <div className="container">
-            <h1>Trivia Rush</h1>
-            <Link to='categories'><div className="playBtn">Play</div></Link>
-            <p className="subText">Tap to Play!</p>
+            <div className="heading">
+                <h1>Trivia Rush</h1>
+            </div>
+            <div className="playBtnDiv">
+                <Link to='categories'>
+                    <div className="playBtn">
+                        <FontAwesomeIcon className='playIcon' icon={faPlay} />
+                    </div>
+                </Link>
+            </div>
+            <div className="subTextDiv">
+                <p className="subText">Tap to Play!</p>
+            </div>
             <div className="btnGroup">
                 <Link to='stats'><button>Stats</button></Link>
                 <Link to='settings'><button>Settings</button></Link>
