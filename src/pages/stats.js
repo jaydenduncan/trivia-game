604ac5567f063b8b1f7ff0036702cea20451c7a5
Jayden Duncan <jaydendncn2019@gmail.com>
2025-04-24 11:02:03 -0500

    Added stats labels and numbers

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -7,36 +7,172 @@ function Stats(){
                 <h1>Stats</h1>
 
                 <div className="categoryStats">
-                    <div className="generalStats">
-                        
+                    <div className="cs generalStats">
+                        <header className="generalHeader">
+                            <h4>General</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="artStats">
-
+                    <div className="cs artStats">
+                        <header className="artHeader">
+                            <h4>Art</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="scienceStats">
-
+                    <div className="cs scienceStats">
+                        <header className="scienceHeader">
+                            <h4>Science</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="geographyStats">
-
+                    <div className="cs geographyStats">
+                        <header className="geographyHeader">
+                            <h4>Geography</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="historyStats">
-
+                    <div className="cs historyStats">
+                        <header className="historyHeader">
+                            <h4>History</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="entertainmentStats">
-
+                    <div className="cs entertainmentStats">
+                        <header className="entertainmentHeader">
+                            <h4>Entertainment</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="mathStats">
-
+                    <div className="cs mathStats">
+                        <header className="mathHeader">
+                            <h4>Math</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
 
-                    <div className="sportsStats">
-
+                    <div className="cs sportsStats">
+                        <header className="sportsHeader">
+                            <h4>Sports</h4>
+                        </header>
+                        <hr className="headerSep"/>
+                        <div className="statsSec gamesPlayedSec">
+                            <p className="statsLabel gamesPlayedLabel">Games Played</p>
+                            <p className="statsNum gamesPlayedNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec highScoreSec">
+                            <p className="statsLabel highScoreLabel">High Score</p>
+                            <p className="statsNum highScoreNum">0</p>
+                        </div>
+                        <hr/>
+                        <div className="statsSec longestStreakSec">
+                            <p className="statsLabel longestStreakLabel">Longest Streak</p>
+                            <p className="statsNum longestStreakNum">0</p>
+                        </div>
                     </div>
                 </div>
             </div>
