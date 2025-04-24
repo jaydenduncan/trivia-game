060055e01329e95f4a2f317b079c05a7be9e0044
Jayden Duncan <jaydendncn2019@gmail.com>
2025-04-23 20:41:19 -0500

    Added stats divs

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,6 +1,46 @@
+import "../css/stats.css";
+
 function Stats(){
     return (
-        <h1>Stats Page</h1>
+        <div className="container">
+            <div className="statsPage">
+                <h1>Stats</h1>
+
+                <div className="categoryStats">
+                    <div className="generalStats">
+                        
+                    </div>
+
+                    <div className="artStats">
+
+                    </div>
+
+                    <div className="scienceStats">
+
+                    </div>
+
+                    <div className="geographyStats">
+
+                    </div>
+
+                    <div className="historyStats">
+
+                    </div>
+
+                    <div className="entertainmentStats">
+
+                    </div>
+
+                    <div className="mathStats">
+
+                    </div>
+
+                    <div className="sportsStats">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
