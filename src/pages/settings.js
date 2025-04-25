48e77fff28fcb3dd2f4a65cc44042847b9e2c334
Jayden Duncan <jaydendncn2019@gmail.com>
2025-04-25 10:48:01 -0500

    Setup settings page

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,6 +1,31 @@
+import "../css/settings.css";
+
 function Settings(){
     return (
-        <h1>Settings Page</h1>
+        <div className="container">
+            <div className="settingsPage">
+                <h1>Settings</h1>
+
+                <div className="settingsDiv">
+                    <div className="difficulty">
+                        <h4 className="difficultyLabel">Difficulty</h4>
+                        <div className="difficultyOptions">
+                            <div className="easyOption"><p>EASY</p></div>
+                            <div className="normalOption"><p>NORMAL</p></div>
+                            <div className="hardOption"><p>HARD</p></div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="gameSpeed">
+                        <h4 className="gameSpeedLabel">Game Speed</h4>
+                    </div>
+                    <hr/>
+                    <div className="theme">
+                        <h4 className="themeLabel">Theme</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
