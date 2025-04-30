1ede47550048cc15af33787262bead3cdac2eba8
Jayden Duncan <jaydendncn2019@gmail.com>
2025-04-30 00:13:45 -0500

    Settings page options added

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -9,7 +9,7 @@ function Settings(){
                 <div className="settingsDiv">
                     <div className="difficulty">
                         <h4 className="difficultyLabel">Difficulty</h4>
-                        <div className="difficultyOptions">
+                        <div className="setOpts difficultyOptions">
                             <div className="easyOption"><p>EASY</p></div>
                             <div className="normalOption"><p>NORMAL</p></div>
                             <div className="hardOption"><p>HARD</p></div>
@@ -18,10 +18,19 @@ function Settings(){
                     <hr/>
                     <div className="gameSpeed">
                         <h4 className="gameSpeedLabel">Game Speed</h4>
+                        <div className="setOpts gameSpeedOptions">
+                            <div className="fastOption"><p>10 SEC</p></div>
+                            <div className="mediumOption"><p>15 SEC</p></div>
+                            <div className="slowOption"><p>20 SEC</p></div>
+                        </div>
                     </div>
                     <hr/>
                     <div className="theme">
                         <h4 className="themeLabel">Theme</h4>
+                        <div className="setOpts themeOptions">
+                            <div className="lightOption"><p>Light</p></div>
+                            <div className="darkOption"><p>Dark</p></div>
+                        </div>
                     </div>
                 </div>
             </div>
