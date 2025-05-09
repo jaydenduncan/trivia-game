39ef9c028bb5514d03d25141842174866c64d6d3
Jayden Duncan <jaydendncn2019@gmail.com>
2025-05-08 20:49:45 -0500

    Reflect settings changes in db

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Settings(){
-    const [settings, setSettings] = useState();
+    const [settings, setSettings] = useState({difficulty: "", game_speed: "", theme: ""});
 
     const initializeSettings = async () => {
         await fetch("/settings")
@@ -14,9 +14,133 @@ function Settings(){
         .catch(err => console.log(err));
     };
 
+    const changeSettingBG = () => {
+        let difficultyOptions = document.getElementsByClassName("difficultyOption");
+        let gameSpeedOptions = document.getElementsByClassName("gameSpeedOption");
+        let themeOptions = document.getElementsByClassName("themeOption");
+
+        // Clear background color of each difficulty option
+        for(let i=0; i<difficultyOptions.length; i++){
+            difficultyOptions[i].style.backgroundColor = '';
+        }
+
+        // Clear background color of each game speed option
+        for(let i=0; i<gameSpeedOptions.length; i++){
+            gameSpeedOptions[i].style.backgroundColor = '';
+        }
+
+        // Clear background color of each theme option
+        for(let i=0; i<themeOptions.length; i++){
+            themeOptions[i].style.backgroundColor = '';
+        }
+
+        // Highlight the chosen difficulty option
+        switch(settings.difficulty){
+            case "EASY":
+                difficultyOptions[0].style.backgroundColor = "#969292";
+                break;
+            case "NORMAL":
+                difficultyOptions[1].style.backgroundColor = "#969292";
+                break;
+            case "HARD":
+                difficultyOptions[2].style.backgroundColor = "#969292";
+                break;
+        }
+
+        // Highlight the chosen game speed option
+        switch(settings.game_speed){
+            case "10":
+                gameSpeedOptions[0].style.backgroundColor = "#969292";
+                break;
+            case "15":
+                gameSpeedOptions[1].style.backgroundColor = "#969292";
+                break;
+            case "20":
+                gameSpeedOptions[2].style.backgroundColor = "#969292";
+                break;
+        }
+
+        // Highlight the chosen theme option
+        switch(settings.theme){
+            case "LIGHT":
+                themeOptions[0].style.backgroundColor = "#969292";
+                break;
+            case "DARK":
+                themeOptions[1].style.backgroundColor = "#969292";
+                break;
+        }
+    }
+
+    const changeDifficulty = async (e) => {
+        e.preventDefault();
+        const targetId = e.currentTarget.id;
+        let difficulty = "";
+        if(targetId === "easyOption") difficulty = "EASY";
+        else if(targetId === "normalOption") difficulty = "NORMAL";
+        else if(targetId === "hardOption") difficulty = "HARD";
+
+        // Reflect changes in database
+        await fetch(`/settings/${settings.id}`, {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "difficulty": difficulty
+            })
+        })
+        .then(res => res)
+        .then(data => initializeSettings())
+        .catch(err => console.log(err));
+    }
+
+    const changeGameSpeed = async (e) => {
+        e.preventDefault();
+        const targetId = e.currentTarget.id;
+        let game_speed = "";
+        if(targetId === "fastOption") game_speed = "10";
+        else if(targetId === "mediumOption") game_speed = "15";
+        else if(targetId === "slowOption") game_speed = "20";
+
+        // Reflect changes in database
+        await fetch(`/settings/${settings.id}`, {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "game_speed": game_speed
+            })
+        })
+        .then(res => res)
+        .then(data => initializeSettings())
+        .catch(err => console.log(err));
+    }
+
+    const changeTheme = async (e) => {
+        e.preventDefault();
+        const targetId = e.currentTarget.id;
+        let theme = "";
+        if(targetId === "lightOption") theme = "LIGHT";
+        else if(targetId === "darkOption") theme = "DARK";
+
+        // Reflect changes in database
+        await fetch(`/settings/${settings.id}`, {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "theme": theme
+            })
+        })
+        .then(res => res)
+        .then(data => initializeSettings())
+        .catch(err => console.log(err));
+    }
+
     useEffect(() => {
         initializeSettings();
     }, []);
+
+    useEffect(() => {
+        console.log(settings);
+        changeSettingBG();
+    }, [settings]);
 
     return (
         <div className="container">
@@ -32,26 +156,26 @@ function Settings(){
                     <div className="difficulty">
                         <h4 className="difficultyLabel">Difficulty</h4>
                         <div className="setOpts difficultyOptions">
-                            <div className="easyOption"><p>EASY</p></div>
-                            <div className="normalOption"><p>NORMAL</p></div>
-                            <div className="hardOption"><p>HARD</p></div>
+                            <div id="easyOption" className="difficultyOption easyOption" onClick={changeDifficulty}><p>EASY</p></div>
+                            <div id="normalOption" className="difficultyOption normalOption" onClick={changeDifficulty}><p>NORMAL</p></div>
+                            <div id="hardOption" className="difficultyOption hardOption" onClick={changeDifficulty}><p>HARD</p></div>
                         </div>
                     </div>
                     <hr/>
                     <div className="gameSpeed">
                         <h4 className="gameSpeedLabel">Game Speed</h4>
                         <div className="setOpts gameSpeedOptions">
-                            <div className="fastOption"><p>10 SEC</p></div>
-                            <div className="mediumOption"><p>15 SEC</p></div>
-                            <div className="slowOption"><p>20 SEC</p></div>
+                            <div id="fastOption" className="gameSpeedOption fastOption" onClick={changeGameSpeed}><p>10 SEC</p></div>
+                            <div id="mediumOption" className="gameSpeedOption mediumOption" onClick={changeGameSpeed}><p>15 SEC</p></div>
+                            <div id="slowOption" className="gameSpeedOption slowOption" onClick={changeGameSpeed}><p>20 SEC</p></div>
                         </div>
                     </div>
                     <hr/>
                     <div className="theme">
                         <h4 className="themeLabel">Theme</h4>
                         <div className="setOpts themeOptions">
-                            <div className="lightOption"><p>Light</p></div>
-                            <div className="darkOption"><p>Dark</p></div>
+                            <div id="lightOption" className="themeOption lightOption" onClick={changeTheme}><p>Light</p></div>
+                            <div id="darkOption" className="themeOption darkOption" onClick={changeTheme}><p>Dark</p></div>
                         </div>
                     </div>
                 </div>
